ef302436549ecc350e0a74d00ed34b3d6ee60df5
Lucas Paulo <lucapaulodev@gmail.com>
2023-08-07 19:04:44 -0300

    Installing vitest and writing tests to validate the created play and next actions.

---
+++ src/store/slices/player.spec.ts
@@ -0,0 +1,64 @@
+import {describe, expect, it} from 'vitest'
+import { next, play, player as reducer } from './player'
+
+const exampleInitialState = {
+    course: {
+        modules: [
+            {
+              id: '1',
+              title: 'Iniciando com React',
+              lessons: [
+                { id: 'Jai8w6K_GnY', title: 'CSS Modules', duration: '13:45' },
+                { id: 'w-DW4DhDfcw', title: 'Estilização do Post', duration: '10:05' },
+              ],
+            },
+            {
+              id: '2',
+              title: 'Estrutura da aplicação',
+              lessons: [
+                { id: 'gE48FQXRZ_o', title: 'Componente: Comment', duration: '13:45' },
+                { id: 'Ng_Vk4tBl0g', title: 'Responsividade', duration: '10:05' },
+              ],
+            },
+        ],
+    },
+    currentModuleIndex: 0,
+    currentLessonIndex: 0,
+}
+
+describe('player slice', () => {
+    it('should be able to play', () => {
+        const state = reducer(exampleInitialState, play([0, 1]))
+
+        expect(state.currentModuleIndex).toEqual(0)
+        expect(state.currentLessonIndex).toEqual(1)
+    })
+
+    it('should be able to play next video', () => {
+        const state = reducer(exampleInitialState, next())
+
+        expect(state.currentModuleIndex).toEqual(0)
+        expect(state.currentLessonIndex).toEqual(1)
+    })
+
+    it('should be able to jump to the next module and play the first video of this next module', () => {
+        const state = reducer({
+            ...exampleInitialState,
+            currentLessonIndex: 1
+        }, next())
+
+        expect(state.currentModuleIndex).toEqual(1)
+        expect(state.currentLessonIndex).toEqual(0)
+    })
+
+    it('should not be able to update the current module and current lesson if there is no next current module or current lesson', () => {
+        const state = reducer({
+            ...exampleInitialState,
+            currentLessonIndex: 1,
+            currentModuleIndex: 1
+        }, next())
+
+        expect(state.currentModuleIndex).toEqual(1)
+        expect(state.currentLessonIndex).toEqual(1)
+    })
+})
