04fbcc95369f45e555977406efb51ec160f4a638
Lucas Paulo <lucapaulodev@gmail.com>
2023-08-07 18:06:03 -0300

    Implementing the next() action to play the next video automatically.

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,18 +1,25 @@
 import ReactPlayer from "react-player";
 import { useAppSelector } from "../store";
+import { useDispatch } from "react-redux";
+import { next } from "../store/slices/player";
 
 export function VideoPlayer() {
+    const dispatch = useDispatch()
     const currentVideoId = useAppSelector(state => {
         const {currentModuleIndex, currentLessonIndex, course} = state.player
         return course.modules[currentModuleIndex].lessons[currentLessonIndex].id
     })
-    
+
+    const handleNext = () => dispatch(next())
+
     return (
         <div className="w-full bg-zinc-950 aspect-video">
             <ReactPlayer
                 width="100%"
                 height="100%"
                 controls
+                onEnded={handleNext}
+                playing
                 url={`https://www.youtube.com/watch?v=${currentVideoId}`}
             />
         </div>
